01062e469a1c0b268a9717ee9a71c8100264e309
alec wang <alex0123321@gmail.com>
2020-01-06 09:12:29 +0800

    post request, get request, using Rxjs operators to transform response data

---
+++ src/app/app.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Setting} from './setting';
+import {map} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+
+  constructor(private http: HttpClient, private setting: Setting) {}
+
+  ngOnInit() {
+    this.fetchPosts();
+  }
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    this.http
+      .post(
+        this.setting.connectingString,
+        postData
+      )
+      .subscribe(responseData => {
+        console.log(responseData);
+      });
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.fetchPosts();
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+
+  private fetchPosts(){
+    this.http
+      .get(this.setting.connectingString)
+      .pipe(map(responseData => {
+        const postArray = [];
+        for(const key in responseData){
+          if(responseData.hasOwnProperty(key)){
+            postArray.push({...responseData[key], id: key});
+          }
+        }
+        return postArray;
+      }))
+      .subscribe(posts => {
+        console.log(posts);
+      });
+  }
+}
