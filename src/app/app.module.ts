3eaf1808587731feb81253800078c7c9f2377fad
alec wang <alex0123321@gmail.com>
2020-01-06 11:44:28 +0800

    auth-interceptor service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { Setting } from './setting';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptorService } from './auth-interceptor.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
-  providers: [Setting],
+  providers: [Setting, {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/auth-interceptor.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthInterceptorService implements HttpInterceptor {
+
+  constructor() { }
+  intercept(req: HttpRequest<any>, next: HttpHandler){
+    console.log('Request is on its way');
+    return next.handle(req);
+  }
+}
