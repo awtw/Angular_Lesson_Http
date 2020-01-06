3e17c148f8ab3e67188083a54890a4f7adb25ca5
alec wang <alex0123321@gmail.com>
2020-01-06 13:07:49 +0800

    mutiple interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,16 @@ import { AppComponent } from './app.component';
 import { Setting } from './setting';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptorService } from './auth-interceptor.service';
+import { LoggingInterceptorService } from './logging-interceptor.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
-  providers: [Setting, {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
+  providers: [Setting, {
+    provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true
+  },{
+      provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptorService, multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEventType } from '@angular/common/http';
-import { tap } from 'rxjs/operators';
+import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,17 +9,9 @@ export class AuthInterceptorService implements HttpInterceptor {
 
   constructor() { }
   intercept(req: HttpRequest<any>, next: HttpHandler){
-    console.log('Request is on its way');
-    console.log(req.url);
-    const modifiedRequest = req.clone({headers: req.headers.append('Auth', 'xyz')});
-    return next.handle(modifiedRequest).pipe(
-      tap(event => {
-        console.log(event);
-        if(event.type === HttpEventType.Response){
-          console.log('Response arrived, body data: ');
-          console.log(event.body);
-        }
-      })
-    );
+    const modifiedRequest = req.clone({
+      headers: req.headers.append('Auth', 'xyz')
+    });
+    return next.handle(modifiedRequest);
   }
 }
